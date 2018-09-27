e1400f5b21c8a397d574c5f386be520569196a37
carlosdiazgirol@hotmail.com <carlosdiazgirol@hotmail.com>
2018-09-27 22:39:56 +0200

    refactorizando

--- mars-Rover-Kata/javascripts/app.js
+++ mars-Rover-Kata/javascripts/app.js
@@ -8,6 +8,8 @@ var rover = {
   },
   travelLog: []
 };
+var min = 0;
+var max = 9;
 
 // ======================
 
@@ -56,34 +58,26 @@ function turnRight(rover){
 function moveForward(rover){
   console.log("moveForward was called");
   switch (rover.direction) {
-  case 'N':
-    if (rover.position.y > 0) {
-      rover.position.y = rover.position.y -1;     
-    } else {
-      rover.position.y = rover.position.y + 0;
-    }
-    break;
-  case 'W':
-    if (rover.position.x > 0) {
-      rover.position.x = rover.position.x - 1;
-    } else {
-      rover.position.x = rover.position.x + 0;
-    }
-    break;
-  case 'S':
-    if (rover.position.y < 10) {
-      rover.position.y = rover.position.y + 1;
-    } else {
-      rover.position.y = rover.position.y + 0;
-    }
-    break;
-  case 'E':
-    if (rover.position.x < 10) {
-      rover.position.x = rover.position.x + 1;
-    } else {
-      rover.position.x = rover.position.x + 0;
-    }    
-    break;
+    case 'N':
+      if (rover.position.y > 0) {
+        rover.position.y -= 1;     
+      }
+      break;
+    case 'W':
+      if (rover.position.x > 0) {
+        rover.position.x -= 1;
+      }
+      break;
+    case 'S':
+      if (rover.position.y < 9) {
+        rover.position.y += 1;
+      } 
+      break;
+    case 'E':
+      if (rover.position.x < 9) {
+        rover.position.x += 1;
+      }    
+      break;
   }
 };
 
@@ -91,56 +85,47 @@ function moveBackwards(rover){
   console.log("moveBackWards was called");
   switch (rover.direction) {
   case 'N':
-    if (rover.position.y < 10) {
-      rover.position.y = rover.position.y + 1;     
-    } else {
-      rover.position.y = rover.position.y + 0;
-    }
+    if (rover.position.y < max) {
+      rover.position.y += 1;     
+    } 
     break;
   case 'W':
-    if (rover.position.x < 10) {
-      rover.position.x = rover.position.x + 1;
-    } else {
-      rover.position.x = rover.position.x + 0;
-    }
+    if (rover.position.x < max) {
+      rover.position.x += 1;
+    } 
     break;
   case 'S':
-    if (rover.position.y > 0) {
-      rover.position.y = rover.position.y - 1;
-    } else {
-      rover.position.y = rover.position.y + 0;
-    }
+    if (rover.position.y > min) {
+      rover.position.y -= 1;
+    } 
     break;
   case 'E':
-    if (rover.position.x > 0) {
-      rover.position.x = rover.position.x - 1;
-    } else {
-      rover.position.x = rover.position.x + 0;
-    }    
+    if (rover.position.x > min) {
+      rover.position.x -= 1;
+    }     
     break;
   }
 };
 
 function doMove(move, rover) {
-  if (move == 'l' || move == 'r' || move == 'f' || move == 'b') {
-    switch (move) {
-      case 'l':
-        turnLeft(rover);
-        break;
-      case 'r':
-        turnRight(rover);
-        break;
-      case 'f':
-        moveForward(rover);
-        break;
-      case 'b':
-        moveBackwards(rover);
-        break;
-    }
-  } else {
+  switch (move) {
+    case 'l':
+      turnLeft(rover);
+      break;
+    case 'r':
+      turnRight(rover);
+      break;
+    case 'f':
+      moveForward(rover);
+      break;
+    case 'b':
+      moveBackwards(rover);
+      break;
+    default:
     console.log('error, imposible do all moves');
-  }
-}
+  } 
+} 
+
 
 function doCommands(moves, rover) {
   var directions = moves.split('');
