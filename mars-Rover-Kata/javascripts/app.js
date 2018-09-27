dae1e41f78ad0d8d14cbc806f4ec3944ceeccd58
carlosdiazgirol@hotmail.com <carlosdiazgirol@hotmail.com>
2018-09-27 13:59:47 +0200

    el rover ya gira

--- mars-Rover-Kata/javascripts/app.js
+++ mars-Rover-Kata/javascripts/app.js
@@ -1,50 +1,52 @@
 // Rover Object Goes Here
 // ======================
-var roverDirection = {
-  north: [1, "north"],
-  east:  [2, "east"],
-  south: [3, "south"],
-  west:  [4, "west"]
+var rover = {
+  direction: 'N'
 }
-var newDirection = roverDirection['north'];
-var left = 1;
-var right = 1;
 // ======================
+
 function turnLeft(rover){
+  console.log('turnLeft was called');
 
-  // if (rover == roverDirection['north'] ) {
-  //   newDirection = roverDirection['west'];
-  // } else if (rover == roverDirection['east'] ) {
-  //   newDirection = roverDirection['north'];
-  // } else if (rover == roverDirection['south'] ) {
-  //   newDirection = roverDirection['east'];
-  // } else if (rover == roverDirection['west'] ) {
-  //   newDirection = roverDirection['south'];
-  // } else {
-  //   newDirection = roverDirection['north'];
-  // }
-  return newDirection;
-}
-
-function turnRight(rover){
-  newDirection = newDirection + rover;
-  return newDirection;
-  if (newDirection = 2) { // Este es el valor de North que pasa a East   
-    console.log(newDirection + " hola");
-  } else if (newDirection = 3) {
-    console.log(newDirection + " caracola");
+  var newDirection;
+  switch (rover.direction) {
+    case 'N':
+      newDirection = 'W';
+      break;
+    case 'W':
+      newDirection = 'S';
+      break;
+    case 'S':
+      newDirection = 'E';
+      break;
+    case 'E':
+      newDirection = 'N';
+      break;
   }
+  rover.direction = newDirection;
 }
 
+function turnRight(rover){ 
+  console.log('turnRight was called');
 
-
-
-
-console.log(newDirection)
-
-
+  var newDirection;
+  switch (rover.direction) {
+    case 'N':
+      newDirection = 'E';
+      break;
+    case 'W':
+      newDirection = 'N';
+      break;
+    case 'S':
+      newDirection = 'W';
+      break;
+    case 'E':
+      newDirection = 'S';
+      break;
+  }
+  rover.direction = newDirection;
+}
 
 function moveForward(rover){
   console.log("moveForward was called")
 }
-
