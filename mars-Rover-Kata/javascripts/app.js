649d626cd97b49ec20b161a60c4b36fa09051896
carlosdiazgirol@hotmail.com <carlosdiazgirol@hotmail.com>
2018-09-27 21:33:37 +0200

    hecho backwards y validate inputs

--- mars-Rover-Kata/javascripts/app.js
+++ mars-Rover-Kata/javascripts/app.js
@@ -87,24 +87,65 @@ function moveForward(rover){
   }
 };
 
+function moveBackwards(rover){
+  console.log("moveBackWards was called");
+  switch (rover.direction) {
+  case 'N':
+    if (rover.position.y < 10) {
+      rover.position.y = rover.position.y + 1;     
+    } else {
+      rover.position.y = rover.position.y + 0;
+    }
+    break;
+  case 'W':
+    if (rover.position.x < 10) {
+      rover.position.x = rover.position.x + 1;
+    } else {
+      rover.position.x = rover.position.x + 0;
+    }
+    break;
+  case 'S':
+    if (rover.position.y > 0) {
+      rover.position.y = rover.position.y - 1;
+    } else {
+      rover.position.y = rover.position.y + 0;
+    }
+    break;
+  case 'E':
+    if (rover.position.x > 0) {
+      rover.position.x = rover.position.x - 1;
+    } else {
+      rover.position.x = rover.position.x + 0;
+    }    
+    break;
+  }
+};
+
 function doMove(move, rover) {
-  switch (move) {
-    case 'l':
-      turnLeft(rover);
-      break;
-    case 'r':
-      turnRight(rover);
-      break;
-    case 'f':
-      moveForward(rover);
-      break;
+  if (move == 'l' || move == 'r' || move == 'f' || move == 'b') {
+    switch (move) {
+      case 'l':
+        turnLeft(rover);
+        break;
+      case 'r':
+        turnRight(rover);
+        break;
+      case 'f':
+        moveForward(rover);
+        break;
+      case 'b':
+        moveBackwards(rover);
+        break;
+    }
+  } else {
+    console.log('error, imposible do all moves');
   }
 }
 
 function doCommands(moves, rover) {
-  var letras = moves.split('');
-  for (i = 0; i < letras.length; i++) {
-    var move = letras[i];
+  var directions = moves.split('');
+  for (i = 0; i < directions.length; i++) {
+    var move = directions[i];
     doMove(move, rover);
     rover.travelLog.push(move);
   }
